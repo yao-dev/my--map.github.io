7f7249c4e3e5176fdfe28a9c32418a586550d4f6
Michael <yao.wilfried@gmail.com>
2015-12-30 14:36:43 +0100

    Update place.php

--- place.php
+++ place.php
@@ -1,4 +1,4 @@
 <?php
 if(isset($_POST['place_id'])){
-	echo file_get_contents('https://maps.googleapis.com/maps/api/place/details/json?placeid='.$_POST["place_id"].'&key=AIzaSyDniw5rqwX1rkney9WfOq7B3JIExdaJfiE');
+	echo file_get_contents('https://maps.googleapis.com/maps/api/place/details/json?placeid='.$_POST["place_id"].'&key=API_KEY');
 }
